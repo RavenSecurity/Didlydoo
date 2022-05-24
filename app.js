3864bf09ff730d55e0b7a610185f978b9c62f41d
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-05-24 14:51:18 +0200

    galère

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const getAttendees = async () => {
     // Generate tables dynamically 
     function generateTables(data) {
         for (let i = 0; i < data.length; i++) {
+
+            //console.log(data[i])
             todo = document.getElementById("todo")
 
             // Creating the basis of the event (table + name of event + description)
@@ -30,6 +32,17 @@ const getAttendees = async () => {
             let whiteHead = document.createElement("th")
             table.appendChild(whiteSpace)
             whiteSpace.appendChild(whiteHead)
+
+
+            for(let date=0;date<data[i].dates.length;date++){
+                console.log(`data[${i}].dates[${date}]`)
+                console.log(data[i].dates[date])
+                for(let attendee=0; attendee < data[i].dates[date].attendees.length;attendee++ ){
+                    console.log(`data[${i}].dates[${date}].attendee[${attendee}]`)
+                    console.log(data[i].dates[date].attendees[attendee])
+
+                }
+            }
             
             // DYNAMICALLY ADD THE DATA IN THE HTML
             for (let k = 0; k < data[i].dates[0].attendees.length; k++) {
@@ -38,36 +51,55 @@ const getAttendees = async () => {
                 td.innerHTML = data[i].dates[0].attendees[k].name
                 table.appendChild(tr)
                 tr.appendChild(td)
+
+                //console.log(data[i].dates[i].attendees[0].available);
+
+                for (let x = 0; x < data[i].dates.length; x++) {
+                    //console.log('bonjour')
+                    //console.log(i, k, x, data[i].dates[0].attendees[k].available.toString())
+                    let presence = document.createElement("td")
+                    presence.innerHTML = data[i].dates[0].attendees[k].available.toString()
+                    tr.appendChild(presence)
+                    //console.log(presence)
+                }
             }
 
             for (let j = 0; j < data[i].dates.length; j++) {
                 let th = document.createElement("th")
                 th.innerHTML = data[i].dates[j].date
                 whiteSpace.appendChild(th)
+                
 
-                for (let g = 0; g < data[i].dates[0].attendees.length; g++) {
-                    let td = document.createElement("td")
-                    if (data[i].dates[0].attendees[g].available == true) {
-                        td.innerHTML = "Yes";
-                    } else td.innerHTML = "No";
+                
                     
                     // table.appendChild(td)
 
-                    console.log(data[i].dates[0].attendees[g].available);
-                }
+                    // console.log(data[i].dates[0].attendees[g].available.toString());
+                
             }
 
 
             // CONFIRM PRESENCE TO THE EVENT
-            let addPersona = document.createElement("tr")
-            table.appendChild(addPersona)
+            let confirmation = ["Yes", "No"]
+            let addPersonaRow = document.createElement("tr")
+            table.appendChild(addPersonaRow)
             let addName = document.createElement("input")
-            addPersona.appendChild(addName)
+            addPersonaRow.appendChild(addName)
 
             for (let j = 0; j < data[i].dates.length; j++) {
-                let addPresence = document.createElement("td")
-                addPresence.innerHTML = "test"
-                addPersona.appendChild(addPresence)
+                let addPresenceD = document.createElement("td")
+                addPersonaRow.appendChild(addPresenceD)
+                let addPresenceSelect = document.createElement("select")
+                addPresenceSelect.innerHTML = "test"
+                addPresenceD.appendChild(addPresenceSelect)
+                
+                //Create and append the options
+                for (let z = 0; z < confirmation.length; z++) {
+                let option = document.createElement("option");
+                option.value = confirmation[z];
+                option.text = confirmation[z];
+                addPresenceSelect.appendChild(option)
+            }
             }
         }
 
